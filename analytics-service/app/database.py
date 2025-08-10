3338e6a4cd746ac3d0f92c6144d61b08201d501c
DivyanjaliTP <chamarishakila@gmail.com>
2025-08-10 14:45:15 +0000

    Initial push to Lugx-Gaming

--- analytics-service/app/database.py
+++ analytics-service/app/database.py
@@ -2,7 +2,7 @@ from clickhouse_connect import get_client
 
 def get_clickhouse_client():
     return get_client(
-        host="clickhouse-server",
+        host="clickhouse",
         port=8123,
         username="default",
         password="",
